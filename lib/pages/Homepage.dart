084c71b70fd8eba49455af4d59c010acea34a445
Ronalmano <pratyush.aryan20@gmail.com>
2023-02-03 11:14:45 +0530

    news api url change

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pacefin_news_app/services/news_class.dart';
+import 'package:pacefin_news_app/services/news_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Future<NewsHeadlines> nhl;
+
+  @override
+  void initstate() {
+    super.initState();
+    nhl = newsService().getNews();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +32,20 @@ class _HomePageState extends State<HomePage> {
         ),
         elevation: 0.0,
         backgroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          Text("Body"),
+          FutureBuilder(
+              future: nhl,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text("Body");
+                } else {
+                  return CircularProgressIndicator();
+                }
+              })
+        ],
       ),
       backgroundColor: Color(0xff464646),
     );
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -5,11 +5,11 @@ import 'package:http/http.dart' as http;
 import 'package:pacefin_news_app/services/news_class.dart';
 
 class newsService {
-  Future getNews() async {
+  Future<NewsHeadlines> getNews() async {
     dynamic nhl;
     var client = http.Client();
     var url = Uri.parse(
-        "https://newsapi.org/v2/everything?q=tesla&from=2022-12-28&sortBy=publishedAt&apiKey=634ac0d0c9044814a32eb5d360b5202f");
+        "https://newsapi.org/v2/everything?q=tesla&from=2023-01-03&sortBy=publishedAt&apiKey=0b31ae75f1ed40ce9f3b5c742196034c");
     var response = await client.get(
       url,
     );
@@ -20,6 +20,6 @@ class newsService {
       nhl = NewsHeadlines.fromJson(res);
       return nhl;
     }
-    return null;
+    return nhl;
   }
 }
